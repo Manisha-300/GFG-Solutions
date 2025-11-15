f8be1f95e1d02287f49802727e3993d6fed06e70
mnmn32 <manisha.sharmaforbusiness@gmail.com>
2025-11-16 01:30:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sort a String/sort-a-string.java
@@ -0,0 +1,13 @@
+// User function Template for Java
+class Solution {
+    String sort(String s) {
+        // code here
+       char[] chars = s.toCharArray();
+    
+  
+    Arrays.sort(chars);
+    
+   
+    return new String(chars);
+    }
+}
